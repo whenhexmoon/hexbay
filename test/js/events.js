cb52e1b7745a406cfd5542d2925717d34e4f180f
whenhexmoon <whenhexmoon@protonmail.com>
2021-05-02 14:09:44 +0200

    Update events.js

--- test/js/events.js
+++ test/js/events.js
@@ -112,10 +112,6 @@ function initReferralUsed() {
 	referralEvents.watch(function(error, result) {
 		console.log(result);
 	});
-	
-	/*referralEvents.get(function(error, result) {
-		console.log(result);
-	});*/
 }
 
 function initActionFrameUnlockEvents() {
