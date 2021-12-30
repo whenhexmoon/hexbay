87cea36b99bd912ce047f9e4e462616af253da6b
whenhexmoon <whenhexmoon@protonmail.com>
2021-12-30 13:47:33 +0100

    Update bundle.js

--- test/bundle.js
+++ test/bundle.js
@@ -1,7 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const Web3 = require("web3");
 
-const ethEnabled = async () => {
+async function connect() {
   if (window.ethereum) {
     await window.ethereum.send('eth_requestAccounts');
     window.web3 = new Web3(window.ethereum);
@@ -11,6 +11,8 @@ const ethEnabled = async () => {
   console.log(false);
   return false;
 }
+
+connect();
 },{"web3":401}],2:[function(require,module,exports){
 module.exports={
     "name": "goerli",
